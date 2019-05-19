ece591f0c2d6073f3e5c7af98ccd6f1756bfe5d0
Mohini Modi <Mohini.Modi@resmed.com>
2019-05-19 13:45:15 -0700

    taking user input

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 The app take in orders from customers and deplete stock from the store's inventory.  App also track product sales across the store's departments and then provide a summary of the highest-grossing departments in the store.
 
 
-to run program
-$npm i
-$ npm init
-$ npm install mysql
-$ npm install require
-$
+To run program make sure to install npm packages as mentioned below from bash terminal: 
+* $ npm i
+* $ npm init
+* $ npm install mysql
+* $ npm install require
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -21,9 +22,32 @@ connection.connect(function(err) {
 });
 
 function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT item_id, product_name, department_name, year, price,stock_quantity FROM products", function(err, res) {
     if (err) throw err;
     console.table(res);
-    connection.end();
+    question1();    
   });
 }
+
+  function question1() {
+    inquirer
+      .prompt({
+        name: "item_id",
+        type: "input",
+        message: "What ID of the product they would like to buy?"
+      })
+      .then(function(answer) {
+        var query = "SELECT item_id, product_name, department_name, year, price,stock_quantity FROM products WHERE ?";
+        connection.query(query,answer,function(err, res) {
+            if (err) throw err;
+            console.table(res);          
+          connection.end();
+        });
+      });
+  }
+
+
+
+
+/* The first should ask them the ID of the product they would like to buy.
+The second message should ask how many units of the product they would like to buy. */
