834dfde2281f059413c1b699b69840f307e692d4
Mohini Modi <Mohini.Modi@resmed.com>
2019-05-20 15:52:00 -0700

    manager choices added

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,82 +18,44 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
+    runSearch();
 });
 
-function afterConnection() {
-    connection.query("SELECT item_id, product_name, department_name, year, price,stock_quantity FROM products", function (err, res) {
-        if (err) throw err;
-        console.table(res);
-        question1();
-    });
-}
-
-function question1() {
+function runSearch() {
     inquirer
-        .prompt(
-            {
-            name: "item_id",
-            type: "input",
-            message: "What Item_ID of the product you would like to buy?"
-        })
-        .then(function (answer1) {
-            var query = "SELECT item_id, product_name, department_name, year, price,stock_quantity FROM products WHERE ?";
-            connection.query(query, answer1, function (err, res) {
-                if (err) throw err;
-                console.table(res);
-                question2();
-            });
-        });
-        
-}
-function question2() {
-    inquirer
-        .prompt(
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "Dear Manager, /n What would you like to do?",
+        choices: [
+          "View Products for Sale",
+          "View Low Inventory",
+          "Add to Inventory",
+          "Add New Product",
+          "exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View Products for Sale":
+          saleSearch();
+          break;
+  
+        case "View Low Inventory":
+          lowInventorySearch();
+          break;
+  
+        case "Add to Inventory":
+          AddInventory();
+          break;
+  
+        case "Add New Product":
+          AddNewProduct();
+          break;
             
-            {
-
-                name: "stock_quantity",
-                type: "input",
-                message: "How many units of the product you would like to buy?",
-                validate: function(value) {
-                    if (isNaN(value) === false) {
-                   
-                    return true;
-                    }
-                    return false;
-                  }
-            }
-        )
-        .then(function (answer2) {
-           // var query = "select price*stock_quantity as cost from products";
-
-            var query ="select * from products where item_id=5 and stock_quantity >?";
-            connection.query(query, [answer2.stock_quantity], function (err, res) {
-                if (err) throw err;
-                console.table(res);
-            
-
-            });            
-        });
-}
-
-
-
-
-/* Create a new Node application called bamazonManager.js. Running this application will:
-
-
-List a set of menu options:
-View Products for Sale
-View Low Inventory
-Add to Inventory
-Add New Product
-If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
-If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
-If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
-If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
- */
-
-
-
+        case "exit":
+          connection.end();
+          break;
+        }
+      });
+  }
--- products.sql
+++ products.sql
@@ -20,3 +20,20 @@ INSERT INTO products ( item_id, product_name, department_name, year, price,stock
 VALUES (7, "Macy", "Dress", 2019, 80, 70);
 INSERT INTO products ( item_id, product_name, department_name, year, price,stock_quantity)
 VALUES (10, "channel", "Perfume", 2015, 650, 30);
+
+# Manager queries 
+
+#If a manager selects View Products for Sale, the app should list every avaitem_idilable item: the item IDs, names, prices, and quantities.
+USE bamazon;
+SELECT * FROM products;
+
+#If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+SELECT * FROM products order by stock_quantity;
+SELECT * FROM products order by price;
+
+#If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+UPDATE products SET stock_quantity = 7 WHERE item_id = 1;
+
+#If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
+INSERT INTO products ( product_name, department_name, year, price,stock_quantity)
+VALUES ("Macy", "Dress", 2019, 10, 5); 
